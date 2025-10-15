71e088dc130a363cbec34a4d732ac7cde79617bb
nkzk <nkzk95@gmail.com>
2025-10-15 23:56:48 +0200

    chore: restructure the code

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -1,51 +1,35 @@
-package main
+package ui
 
 import (
 	"fmt"
 	"os"
-	"reflect"
 	"time"
 
 	viewport "github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/table"
+	"gopkg.in/yaml.v3"
 )
 
-type model struct {
-	table        *table.Table
-	rows         [][]string
-	cursor       int
-	err          error
-	client       Client
-	viewport     viewport.Model
-	showViewport bool
-}
+type errMsg struct{ err error }
 
-type row struct {
-	Namespace  string
-	Kind       string
-	ApiVersion string
-	Name       string
-	Synced     string
-	Ready      string
-}
+func (e errMsg) Error() string { return e.err.Error() }
 
-func NewModel() *model {
+func NewModel() *Model {
 	r := row{}
 	headers := headersFromRow(r)
 	rows := [][]string{}
 
 	re := lipgloss.NewRenderer(os.Stdout)
-
 	baseStyle := re.NewStyle().Padding(0, 1)
-
 	selectedStyle := baseStyle.Foreground(lipgloss.Color("#01BE85")).Background(lipgloss.Color("#00E2C7"))
 
-	m := &model{
+	m := &Model{
 		viewport: viewport.New(0, 0),
 		client:   mock{},
 	}
+
 	t := table.New().
 		Headers(headers...).
 		Rows(rows...).
@@ -55,7 +39,6 @@ func NewModel() *model {
 			if row == m.cursor {
 				return selectedStyle
 			}
-
 			return baseStyle
 		}).
 		Border(lipgloss.ThickBorder())
@@ -64,18 +47,11 @@ func NewModel() *model {
 	return m
 }
 
-type errMsg struct{ err error }
-
-func (e errMsg) Error() string { return e.err.Error() }
-
-func getRefs(yamlString string) tea.Cmd {
-	return func() tea.Msg {
-		refs, err := getRows(yamlString)
-		if err != nil {
-			return errMsg{err: err}
-		}
-		return refs
-	}
+func (m *Model) Init() tea.Cmd {
+	return tea.Batch(
+		getRefs(m.client.GetXRD()),
+		tick(),
+	)
 }
 
 const refreshInterval = 7 * time.Second
@@ -88,88 +64,38 @@ func tick() tea.Cmd {
 	})
 }
 
-func (m *model) Init() tea.Cmd {
-	return tea.Batch(
-		getRefs(m.client.GetXRD()),
-		tick(),
-	)
-}
-
-func headersFromRow(r row) []string {
-	v := reflect.ValueOf(r)
-	t := v.Type()
-	out := make([]string, v.NumField())
-	for i := 0; i < v.NumField(); i++ {
-		out[i] = t.Field(i).Name
-	}
-	return out
-}
-
-func toStringRow(r row) []string {
-	return []string{r.Namespace, r.Kind, r.ApiVersion, r.Name, r.Synced, r.Ready}
-}
-
-func toRow(s []string) (row, error) {
-	var r row
-	v := reflect.ValueOf(r)
-
-	if len(s) != v.NumField() {
-		return row{}, fmt.Errorf("row has %d fields but only %d is allowed", len(s), v.NumField())
-	}
-
-	rv := reflect.ValueOf(&r).Elem()
-	for i := 0; i < rv.NumField(); i++ {
-		v.Type().Field(i)
-		f := rv.Field(i)
-		sf := v.Field(i)
-
-		if !f.CanSet() {
-			return row{}, fmt.Errorf("field %q cannot be set (make it exported)", sf.Type().Name())
-		}
-
-		if f.Kind() != reflect.String {
-			return row{}, fmt.Errorf("field %q is %s, need string", sf.Type().Name, f.Kind())
-		}
-
-		f.SetString(s[i])
-	}
-
-	return r, nil
-}
-func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tickMsg:
 		return m, tea.Batch(
 			getRefs(m.client.GetXRD()),
 			tick(),
 		)
+
 	case []row:
 		m.applyData(msg)
+
 	case tea.WindowSizeMsg:
 		if m.table != nil {
 			m.table = m.table.Width(msg.Width).Height(msg.Height)
 		}
 		m.viewport.Width = msg.Width
 		m.viewport.Height = msg.Height
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc", "backspace":
 			m.showViewport = false
 		case "q", "ctrl+c":
 			return m, tea.Quit
-
 		case "enter":
 			r := m.rows[m.cursor]
 			row, err := toRow(r)
 			if err != nil {
 				m.err = fmt.Errorf("failed to convert row string to row: %w", err)
 			}
-
 			result := m.client.Get(row.Kind, row.ApiVersion, row.Name, row.Namespace)
-
 			m.viewport.SetContent(result)
-
 			m.showViewport = true
 		case "up", "k":
 			if m.cursor > 0 {
@@ -192,7 +118,7 @@ func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m *model) View() string {
+func (m *Model) View() string {
 	s := "\n"
 
 	if m.err != nil {
@@ -205,19 +131,17 @@ func (m *model) View() string {
 
 	if m.showViewport {
 		s += m.viewport.View()
-
 	} else {
 		s += m.table.String() + "\n"
 	}
 	return s
 }
 
-func (m *model) applyData(newRows []row) {
+func (m *Model) applyData(newRows []row) {
 	if len(newRows) == 0 {
 		return
 	}
 
-	// just reset for now
 	m.table.ClearRows()
 	m.rows = [][]string{}
 
@@ -228,4 +152,36 @@ func (m *model) applyData(newRows []row) {
 
 	m.rows = rows
 	m.table.Rows(rows...)
+}
+
+func getRows(yamlString string) ([]row, error) {
+	xr := &XR{}
+	err := yaml.Unmarshal([]byte(yamlString), xr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal xr: %w", err)
+	}
+
+	var result []row
+	for _, resourceRef := range xr.Spec.Crossplane.ResourceRefs {
+		result = append(result, row{
+			Namespace:  xr.Metadata.Namespace,
+			Kind:       resourceRef.Kind,
+			ApiVersion: resourceRef.ApiVersion,
+			Name:       resourceRef.Name,
+			Synced:     "True",
+			Ready:      "True",
+		})
+	}
+
+	return result, nil
+}
+
+func getRefs(yamlString string) tea.Cmd {
+	return func() tea.Msg {
+		refs, err := getRows(yamlString)
+		if err != nil {
+			return errMsg{err: err}
+		}
+		return refs
+	}
 }
--- main.go
+++ main.go
@@ -5,53 +5,11 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"gopkg.in/yaml.v3"
+	"github.com/nkzk/xtree/internal/ui"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
-type resourceRef struct {
-	ApiVersion string `json:"apiVersion" yaml:"apiVersion"`
-	Kind       string `json:"kind" yaml:"kind"`
-	Name       string `json:"name" yaml:"name"`
-}
-
-type XR struct {
-	Metadata struct {
-		Namespace string `json:"namespace" yaml:"namespace"`
-	} `json:"metadata" yaml:"metadata"`
-	Spec struct {
-		Crossplane struct {
-			ResourceRefs []resourceRef `json:"resourceRefs" yaml:"resourceRefs"`
-		} `json:"crossplane" yaml:"crossplane"`
-	} `json:"spec" yaml:"spec"`
-}
-
-func getRows(yamlString string) ([]row, error) {
-	xr := &XR{}
-	err := yaml.Unmarshal([]byte(yamlString), xr)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal xr: %w", err)
-	}
-
-	var result []row
-	for _, resourceRef := range xr.Spec.Crossplane.ResourceRefs {
-		result = append(result, row{
-			Namespace:  xr.Metadata.Namespace,
-			Kind:       resourceRef.Kind,
-			ApiVersion: resourceRef.ApiVersion,
-			Name:       resourceRef.Name,
-			Synced:     "True",
-			Ready:      "True",
-		})
-	}
-
-	return result, nil
-}
-
 func main() {
-	if _, err := tea.NewProgram(NewModel(), tea.WithAltScreen()).Run(); err != nil {
+	if _, err := tea.NewProgram(ui.NewModel(), tea.WithAltScreen()).Run(); err != nil {
 		fmt.Printf("failed to start: %v", err)
 		os.Exit(1)
 	}
