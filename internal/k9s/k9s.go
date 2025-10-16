54705cd7b63be11061ccdf83b9ebbfe5fee10fb8
nkzk <nkzk95@gmail.com>
2025-10-16 22:35:21 +0200

    chore: add k9s install logic

--- internal/k9s/k9s.go
+++ internal/k9s/k9s.go
@@ -6,13 +6,26 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/nkzk/xtree/internal/utils"
+	"gopkg.in/yaml.v3"
 )
 
-func Install() error {
+func Install(shortcut string) error {
+	const (
+		pluginKey   = "xrefs"
+		description = "Show XR resourceRefs"
+	)
+
+	executablePath, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("failed to get executable path: %w", err)
+	}
+
 	dependencies := []string{"go", "k9s"}
 	for _, dep := range dependencies {
 		if !installed(dep) {
@@ -25,12 +38,74 @@ func Install() error {
 		return fmt.Errorf("failed to get k9s info: %w", err)
 	}
 
-	dir, err := getPluginDirectory(output)
+	pluginPath, err := getPluginDirectory(output)
 	if err != nil {
 		return fmt.Errorf("failed to get plugin directory: %w", err)
 	}
 
-	fmt.Printf("%s", dir)
+	background := false
+	scopes := []string{"all"}
+
+	if !fileExists(pluginPath) {
+		if err := os.MkdirAll(filepath.Dir(pluginPath), 0o755); err != nil {
+			return fmt.Errorf("mkdir for %s: %w", pluginPath, err)
+		}
+
+		root := yaml.Node{
+			Kind: yaml.DocumentNode,
+			Content: []*yaml.Node{
+				{Kind: yaml.MappingNode},
+			},
+		}
+
+		top := root.Content[0]
+
+		top.Content = append(top.Content,
+			&yaml.Node{Kind: yaml.ScalarNode, Tag: "!!str", Value: "plugins"},
+			&yaml.Node{Kind: yaml.MappingNode},
+		)
+
+		bin, err := yaml.Marshal(&root)
+		if err != nil {
+			return fmt.Errorf("marshal seed yaml: %w", err)
+		}
+
+		out, err := appendPlugin(bin, pluginKey, shortcut, executablePath, description, background, scopes)
+		if err != nil {
+			return fmt.Errorf("append plugin into new file: %w", err)
+		}
+
+		if err := os.WriteFile(pluginPath, out, 0o644); err != nil {
+			return fmt.Errorf("write new plugins file: %w", err)
+		}
+
+		return nil
+	}
+
+	if err := backupFile(pluginPath); err != nil {
+		return fmt.Errorf("failed to backup file: %w", err)
+	}
+
+	in, err := os.ReadFile(pluginPath)
+	if err != nil {
+		return fmt.Errorf("read %s: %w", pluginPath, err)
+	}
+
+	if len(in) == 0 {
+		in = []byte("plugins: {}\n")
+	}
+
+	out, err := appendPlugin(in, pluginKey, shortcut, executablePath, description, background, scopes)
+	if err != nil {
+		if errors.Is(err, os.ErrExist) {
+			return fmt.Errorf("plugin %q already present", pluginKey)
+		}
+		return fmt.Errorf("append plugin: %w", err)
+	}
+
+	if err := os.WriteFile(pluginPath, out, 0o644); err != nil {
+		return fmt.Errorf("write %s: %w", pluginPath, err)
+	}
 
 	return nil
 }
@@ -47,10 +122,23 @@ func installed(s string) bool {
 func getPluginDirectory(input []byte) (string, error) {
 	scanner := bufio.NewScanner(bytes.NewReader(input))
 	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), "Plugins") {
-			return strings.ReplaceAll(strings.Join(strings.Fields(scanner.Text())[1:], " "), " ", "\\ "), nil
+		line := scanner.Text()
+		if strings.Contains(line, "Plugins") {
+			fields := strings.Fields(line)
+			if len(fields) > 1 {
+				return strings.Join(fields[1:], " "), nil
+			}
+			break
 		}
 	}
+	return "", errors.New("failed to get k9s plugin directory from k9s info")
+}
 
-	return "", errors.New("failed to get k9s plugind directory from k9s info")
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return true
 }
--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+
 	"github.com/nkzk/xtree/internal/config"
+	"github.com/nkzk/xtree/internal/k9s"
 	"github.com/nkzk/xtree/internal/ui"
 )
 
 func main() {
+	var install bool
+	var shortcut string
+
+	flag.BoolVar(&install, "install", false, "Install the k9s plugin")
+	flag.StringVar(&shortcut, "shortcut", "Shift-G", "Shortcut for the plugin (e.g. x, Shift-G, Ctrl-G)")
+	flag.Parse()
+
+	k9s.Install("Shift-G")
+
+	if install {
+		if err := k9s.Install(shortcut); err != nil {
+			fmt.Fprintf(os.Stderr, "install failed: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("plugin installed")
+		return
+	}
+
 	client := ui.NewKubectlClient()
 	config := config.Create()
+
 	if !config.IsValid() {
 		fmt.Printf(
 			"invalid configuration, make sure NAME, NAMESPACE, RESOURCE_NAME, RESOURCE_VERSION environment variables are set\n" +
@@ -19,6 +41,7 @@ func main() {
 				"if you try to run this program outside of k9s you must supply these environment variables manually\n",
 		)
 	}
+
 	if _, err := tea.NewProgram(ui.NewModel(*client, *config), tea.WithAltScreen()).Run(); err != nil {
 		fmt.Printf("failed to start: %v", err)
 		os.Exit(1)
