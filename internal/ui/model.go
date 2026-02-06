2c241cbc9d54d9de4613cb4e9ad8821dfec80a13
Nikita Zakharov <nikita.zakharov@statnett.no>
2026-02-06 14:21:34 +0100

    Revert "gather row types"
    This reverts commit 770ea5ea210f440ab5be306a7a6c5db74e8e6fcc.

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"reflect"
 	"sync"
 
 	"github.com/charmbracelet/bubbles/help"
@@ -52,46 +51,6 @@ type row struct {
 	ReadyReason  string
 }
 
-func headersFromRow(r row) []string {
-	v := reflect.ValueOf(r)
-	t := v.Type()
-	out := make([]string, v.NumField())
-	for i := 0; i < v.NumField(); i++ {
-		out[i] = t.Field(i).Name
-	}
-	return out
-}
-
-func toStringRow(r row) []string {
-	return []string{r.Namespace, r.Kind, r.ApiVersion, r.Name, r.Synced, r.SyncedReason, r.Ready, r.ReadyReason}
-}
-
-func toRow(s []string) (row, error) {
-	var r row
-	v := reflect.ValueOf(r)
-
-	if len(s) != v.NumField() {
-		return row{}, fmt.Errorf("row has %d fields but only %d is allowed", len(s), v.NumField())
-	}
-
-	rv := reflect.ValueOf(&r).Elem()
-	for i := 0; i < rv.NumField(); i++ {
-		v.Type().Field(i)
-		f := rv.Field(i)
-		sf := v.Field(i)
-
-		if !f.CanSet() {
-			return row{}, fmt.Errorf("field %q cannot be set (make it exported)", sf.Type().Name())
-		}
-
-		if f.Kind() != reflect.String {
-			return row{}, fmt.Errorf("field %q is %s, need string", sf.Type().Name(), f.Kind())
-		}
-
-		f.SetString(s[i])
-	}
-	return r, nil
-}
 func NewModel(client Client, config config.Config) *Model {
 	r := row{}
 	headers := headersFromRow(r)
--- internal/ui/types.go
+++ internal/ui/types.go
@@ -1,8 +1,54 @@
 package ui
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type errMsg struct{ err error }
 
 func (e errMsg) Error() string { return e.err.Error() }
+
+func headersFromRow(r row) []string {
+	v := reflect.ValueOf(r)
+	t := v.Type()
+	out := make([]string, v.NumField())
+	for i := 0; i < v.NumField(); i++ {
+		out[i] = t.Field(i).Name
+	}
+	return out
+}
+
+func toStringRow(r row) []string {
+	return []string{r.Namespace, r.Kind, r.ApiVersion, r.Name, r.Synced, r.SyncedReason, r.Ready, r.ReadyReason}
+}
+
+func toRow(s []string) (row, error) {
+	var r row
+	v := reflect.ValueOf(r)
+
+	if len(s) != v.NumField() {
+		return row{}, fmt.Errorf("row has %d fields but only %d is allowed", len(s), v.NumField())
+	}
+
+	rv := reflect.ValueOf(&r).Elem()
+	for i := 0; i < rv.NumField(); i++ {
+		v.Type().Field(i)
+		f := rv.Field(i)
+		sf := v.Field(i)
+
+		if !f.CanSet() {
+			return row{}, fmt.Errorf("field %q cannot be set (make it exported)", sf.Type().Name())
+		}
+
+		if f.Kind() != reflect.String {
+			return row{}, fmt.Errorf("field %q is %s, need string", sf.Type().Name(), f.Kind())
+		}
+
+		f.SetString(s[i])
+	}
+	return r, nil
+}
 
 type resourceRef struct {
 	ApiVersion string `json:"apiVersion" yaml:"apiVersion"`
