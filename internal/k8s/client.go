9df4b637e593f0e91a6fa1d2d2fde78f9bd59f9d
nkzk <nkzk95@gmail.com>
2026-04-21 19:32:26 +0200

    add storage, rename client to resourceloader

--- internal/k8s/client.go
+++ internal/k8s/client.go
@@ -3,28 +3,31 @@ package k8s
 import (
 	"log"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type Client struct {
-	client runtimeclient.Client
-	// store
-	resources *v1.APIResourceList // todo: storage-interface
+type Storage interface{}
+
+type ResourceLoader struct {
+	inMemory bool
+
+	client  runtimeclient.Client
+	storage Storage
 }
 
-func New() *Client {
+func New(inMemory bool, storage Storage) *ResourceLoader {
 	client, err := runtimeclient.New(getKubernetesConfig(), runtimeclient.Options{})
 	if err != nil {
 		log.Fatalf("failed to set up new runtime-client: %v", err)
 	}
 
-	return &Client{
-		client: client,
+	return &ResourceLoader{
+		client:  client,
+		storage: storage,
 	}
 }
 
-func (d *Client) isObjectNamespaced(obj runtime.Object) (bool, error) {
+func (d *ResourceLoader) isObjectNamespaced(obj runtime.Object) (bool, error) {
 	return d.client.IsObjectNamespaced(obj)
 }
---
+++ internal/storage/memory.go
@@ -0,0 +1,8 @@
+package storage
+
+type Memory struct {
+}
+
+func NewMemory() *Memory {
+	return &Memory{}
+}
