08436f14c81c52903fed5555e158b02353411fb3
Nikita Zakharov <nikita.zakharov@statnett.no>
2026-04-27 14:06:09 +0200

    fix: add import alias
    and remove unused code

--- internal/ui/root.go
+++ internal/ui/root.go
@@ -8,7 +8,7 @@ import (
 
 	"charm.land/bubbles/v2/list"
 	tea "charm.land/bubbletea/v2"
-	"charm.land/lipgloss/v2"
+	lipgloss "charm.land/lipgloss/v2"
 	"github.com/nkzk/xrefs/internal/models"
 )
 
